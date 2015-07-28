50c1f3095395ca9069fc774cb0f49c8a1e1a6cb2
kongxiangming <123456>
2015-07-28 17:53:09 +0800

    0

--- src/main/java/com/kxm/kcgl/view/StockView.java
+++ src/main/java/com/kxm/kcgl/view/StockView.java
@@ -1,26 +1,17 @@
 package com.kxm.kcgl.view;
 
-/**
- * @author badqiu email:badqiu(a)gmail.com
- * @version 1.0
- * @since 1.0
- */
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.event.ActionEvent;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.hyjd.frame.psm.datamodel.PaginationDataModel;
-import com.hyjd.frame.psm.utils.MsgTool;
-import com.kxm.kcgl.domain.Stock;
-import com.kxm.kcgl.domain.User;
-import com.kxm.kcgl.service.StockService;
-import com.kxm.kcgl.service.UserService;
+import com.kxm.kcgl.domain.Product;
+import com.kxm.kcgl.service.ProductService;
 
 @Component
 @Scope("view")
@@ -28,36 +19,25 @@ public class StockView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Autowired
-	private StockService stockService;
+	private List<Product> stockList = new ArrayList<Product>();
 
-	private Stock condition = new Stock();
-	
-	private PaginationDataModel<Stock> stockDataModel;
+	@Autowired
+	private ProductService productService;
 
 	@PostConstruct
 	public void init() {
 		queryStock();
 	}
 
-	public void queryStock(){
-		stockDataModel = new PaginationDataModel<Stock>("com.kxm.kcgl.mapper.StockMapper.selectSelective",condition);
-	}
-	
-	
-	public PaginationDataModel<Stock> getStockDataModel() {
-		return stockDataModel;
+	public void queryStock() {
+		stockList = productService.queryAll();
 	}
 
-	public void setStockDataModel(PaginationDataModel<Stock> stockDataModel) {
-		this.stockDataModel = stockDataModel;
+	public List<Product> getStockList() {
+		return stockList;
 	}
 
-	public Stock getCondition() {
-		return condition;
-	}
-
-	public void setCondition(Stock condition) {
-		this.condition = condition;
+	public void setStockList(List<Product> stockList) {
+		this.stockList = stockList;
 	}
 }
