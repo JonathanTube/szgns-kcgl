a821a4da7b42b16181e0d394e16803cd88a4ac4e
kongxm66 <275568635@qq.com>
2015-07-28 21:11:25 +0800

    0

--- src/main/java/com/kxm/kcgl/domain/Product.java
+++ src/main/java/com/kxm/kcgl/domain/Product.java
@@ -6,154 +6,121 @@ public class Product {
 	private Integer id;
 	private String productNo;
 	private String productName;
-	private int brandId;
+	private Integer brandId;
 	private String brandName;
-	private int techId;
+	private Integer techId;
 	private String techName;
-	private int thicknessId;
+	private Integer thicknessId;
 	private String thicknessName;
-	private int identifyId;
+	private Integer identifyId;
 	private String identifyName;
-	private double price;
-	private int amount;
-	private int queryTimes;
-	private int createUserId;
+	private Double price;
+	private Integer amount;
+	private Integer queryTimes;
+	private Integer createUserId;
 	private String createUsername;
 	private Date createTime;
-
 	public Integer getId() {
 		return id;
 	}
-
-	public String getProductNo() {
-		return productNo;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public int getBrandId() {
-		return brandId;
-	}
-
-	public String getBrandName() {
-		return brandName;
-	}
-
-	public int getTechId() {
-		return techId;
-	}
-
-	public String getTechName() {
-		return techName;
-	}
-
-	public int getThicknessId() {
-		return thicknessId;
-	}
-
-	public String getThicknessName() {
-		return thicknessName;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public int getQueryTimes() {
-		return queryTimes;
-	}
-
-	public Date getCreateTime() {
-		return createTime;
-	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	public String getProductNo() {
+		return productNo;
+	}
 	public void setProductNo(String productNo) {
 		this.productNo = productNo;
 	}
-
+	public String getProductName() {
+		return productName;
+	}
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-
-	public void setBrandId(int brandId) {
+	public Integer getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(Integer brandId) {
 		this.brandId = brandId;
 	}
-
+	public String getBrandName() {
+		return brandName;
+	}
 	public void setBrandName(String brandName) {
 		this.brandName = brandName;
 	}
-
-	public void setTechId(int techId) {
+	public Integer getTechId() {
+		return techId;
+	}
+	public void setTechId(Integer techId) {
 		this.techId = techId;
 	}
-
+	public String getTechName() {
+		return techName;
+	}
 	public void setTechName(String techName) {
 		this.techName = techName;
 	}
-
-	public void setThicknessId(int thicknessId) {
+	public Integer getThicknessId() {
+		return thicknessId;
+	}
+	public void setThicknessId(Integer thicknessId) {
 		this.thicknessId = thicknessId;
 	}
-
+	public String getThicknessName() {
+		return thicknessName;
+	}
 	public void setThicknessName(String thicknessName) {
 		this.thicknessName = thicknessName;
 	}
-
-	public void setPrice(double price) {
+	public Integer getIdentifyId() {
+		return identifyId;
+	}
+	public void setIdentifyId(Integer identifyId) {
+		this.identifyId = identifyId;
+	}
+	public String getIdentifyName() {
+		return identifyName;
+	}
+	public void setIdentifyName(String identifyName) {
+		this.identifyName = identifyName;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
 		this.price = price;
 	}
-
-	public void setQueryTimes(int queryTimes) {
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public Integer getQueryTimes() {
+		return queryTimes;
+	}
+	public void setQueryTimes(Integer queryTimes) {
 		this.queryTimes = queryTimes;
 	}
-
+	public Integer getCreateUserId() {
+		return createUserId;
+	}
+	public void setCreateUserId(Integer createUserId) {
+		this.createUserId = createUserId;
+	}
+	public String getCreateUsername() {
+		return createUsername;
+	}
+	public void setCreateUsername(String createUsername) {
+		this.createUsername = createUsername;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
 
-	public int getIdentifyId() {
-		return identifyId;
-	}
-
-	public String getIdentifyName() {
-		return identifyName;
-	}
-
-	public void setIdentifyId(int identifyId) {
-		this.identifyId = identifyId;
-	}
-
-	public void setIdentifyName(String identifyName) {
-		this.identifyName = identifyName;
-	}
-
-	public int getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
-	public int getCreateUserId() {
-		return createUserId;
-	}
-
-	public String getCreateUsername() {
-		return createUsername;
-	}
-
-	public void setCreateUserId(int createUserId) {
-		this.createUserId = createUserId;
-	}
-
-	public void setCreateUsername(String createUsername) {
-		this.createUsername = createUsername;
-	}
 }
--- src/main/java/com/kxm/kcgl/view/QueryTimesView.java
+++ src/main/java/com/kxm/kcgl/view/QueryTimesView.java
@@ -1,14 +1,22 @@
 package com.kxm.kcgl.view;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.hyjd.frame.psm.datamodel.PaginationDataModel;
+import com.kxm.kcgl.domain.BrandBean;
 import com.kxm.kcgl.domain.Product;
+import com.kxm.kcgl.domain.TechBean;
+import com.kxm.kcgl.domain.ThicknessBean;
+import com.kxm.kcgl.service.BrandService;
+import com.kxm.kcgl.service.TechService;
+import com.kxm.kcgl.service.ThicknessService;
 
 @Component
 @Scope("view")
@@ -18,12 +26,39 @@ public class QueryTimesView implements Serializable {
 	private Product condition = new Product();
 	private PaginationDataModel<Product> productDataModel;
 
+	@Autowired
+	private BrandService brandService;
+	private List<BrandBean> brandList;
+
+	@Autowired
+	private TechService techService;
+	private List<TechBean> techList;
+
+	@Autowired
+	private ThicknessService thicknessService;
+	private List<ThicknessBean> thicknessList;
+
 	@PostConstruct
 	public void init() {
-		queryStock();
+		initQuery();
+		initBrandList();
+		initTechList();
+		initThicknessList();
 	}
 
-	public void queryStock() {
+	public void initBrandList() {
+		brandList = brandService.queryAllBrand();
+	}
+
+	public void initTechList() {
+		techList = techService.queryAllTech();
+	}
+	
+	public void initThicknessList() {
+		thicknessList = thicknessService.queryAllThickness();
+	}
+	
+	public void initQuery() {
 		productDataModel = new PaginationDataModel<Product>("com.kxm.kcgl.mapper.ProductMapper.selectSelective", condition);
 	}
 
@@ -41,6 +76,30 @@ public class QueryTimesView implements Serializable {
 
 	public void setCondition(Product condition) {
 		this.condition = condition;
+	}
+
+	public List<BrandBean> getBrandList() {
+		return brandList;
+	}
+
+	public void setBrandList(List<BrandBean> brandList) {
+		this.brandList = brandList;
+	}
+
+	public List<TechBean> getTechList() {
+		return techList;
+	}
+
+	public void setTechList(List<TechBean> techList) {
+		this.techList = techList;
+	}
+
+	public List<ThicknessBean> getThicknessList() {
+		return thicknessList;
+	}
+
+	public void setThicknessList(List<ThicknessBean> thicknessList) {
+		this.thicknessList = thicknessList;
 	}
 
 }
