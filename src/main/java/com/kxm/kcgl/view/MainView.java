4c58fd3d63d20ae6bfbf1f3473b8f079e50dd168
kongxm66 <275568635@qq.com>
2015-07-30 21:35:34 +0800

    10

--- src/main/java/com/kxm/kcgl/view/MainView.java
+++ src/main/java/com/kxm/kcgl/view/MainView.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.hyjd.component.echarts.pie.model.PieChartModel;
 import com.kxm.kcgl.domain.Product;
 import com.kxm.kcgl.domain.Stock;
 import com.kxm.kcgl.service.ProductService;
@@ -24,32 +28,57 @@ public class MainView implements Serializable {
 	@Autowired
 	private ProductService productService;
 
-	private Product product = new Product();
+	private Product selectedProduct = new Product();
 
 	private List<Stock> stockList;
+	
+
+	private PieChartModel model;
+
+	@PostConstruct
+	public void init() {
+		List<Stock> stocks = stockService.selectSelective(new Stock());
+		model = new PieChartModel("库存");
+		for (Stock stock : stocks) {
+			model.set(stock.getProductName(), stock.getAmount());	
+		}
+	}
+
+	public PieChartModel getModel() {
+		return model;
+	}
+
+	public void setModel(PieChartModel model) {
+		this.model = model;
+	}
 
 	public List<Product> completeProduct(String keywords) {
-		List<Product> list = productService.search(keywords);
+		List<Product> list = new ArrayList<Product>();
+		if (StringUtils.isEmpty(keywords))
+			return list;
+		list = productService.search(keywords);
 		return list;
 	}
 
-	public void query() {
+	public void onItemSelect() {
 		Stock condition = new Stock();
+		condition.setProductId(selectedProduct.getId());		
+		stockList = stockService.selectSelective(condition);
 	}
 
+	public Product getSelectedProduct() {
+		return selectedProduct;
+	}
+
+	public void setSelectedProduct(Product selectedProduct) {
+		this.selectedProduct = selectedProduct;
+	}
+	
 	public List<Stock> getStockList() {
 		return stockList;
 	}
 
 	public void setStockList(List<Stock> stockList) {
 		this.stockList = stockList;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
 	}
 }
