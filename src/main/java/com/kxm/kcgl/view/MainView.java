60a4a9711588c5c154166178699c704edead8828
Administrator <Administrator@PC201505171805>
2015-07-30 23:01:04 +0800

    11

--- src/main/java/com/kxm/kcgl/view/MainView.java
+++ src/main/java/com/kxm/kcgl/view/MainView.java
@@ -4,14 +4,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.hyjd.component.echarts.pie.model.PieChartModel;
 import com.kxm.kcgl.domain.Product;
 import com.kxm.kcgl.domain.Stock;
 import com.kxm.kcgl.service.ProductService;
@@ -32,26 +29,6 @@ public class MainView implements Serializable {
 
 	private List<Stock> stockList;
 	
-
-	private PieChartModel model;
-
-	@PostConstruct
-	public void init() {
-		List<Stock> stocks = stockService.selectSelective(new Stock());
-		model = new PieChartModel("库存");
-		for (Stock stock : stocks) {
-			model.set(stock.getProductName(), stock.getAmount());	
-		}
-	}
-
-	public PieChartModel getModel() {
-		return model;
-	}
-
-	public void setModel(PieChartModel model) {
-		this.model = model;
-	}
-
 	public List<Product> completeProduct(String keywords) {
 		List<Product> list = new ArrayList<Product>();
 		if (StringUtils.isEmpty(keywords))
