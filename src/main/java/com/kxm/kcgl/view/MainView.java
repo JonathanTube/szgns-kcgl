678638e51b04aa9b04b5be91c21a0eb2bc3768c5
kongxiangming <123456>
2015-08-10 14:50:07 +0800

    00

--- src/main/java/com/kxm/kcgl/view/MainView.java
+++ src/main/java/com/kxm/kcgl/view/MainView.java
@@ -7,18 +7,24 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang.StringUtils;
+import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.hyjd.frame.psm.base.LoginSession;
 import com.hyjd.frame.psm.datamodel.PaginationDataModel;
+import com.hyjd.frame.psm.utils.MsgTool;
+import com.kxm.kcgl.LogicException;
+import com.kxm.kcgl.domain.PreProductOut;
 import com.kxm.kcgl.domain.Product;
 import com.kxm.kcgl.domain.ProductQueryTimes;
 import com.kxm.kcgl.domain.Stock;
 import com.kxm.kcgl.domain.User;
+import com.kxm.kcgl.service.PreProductOutService;
 import com.kxm.kcgl.service.ProductQueryTimesService;
 import com.kxm.kcgl.service.ProductService;
+import com.kxm.kcgl.service.StockService;
 
 @Component
 @Scope("view")
@@ -33,6 +39,16 @@ public class MainView implements Serializable {
 
 	@Autowired
 	private ProductQueryTimesService productQueryTimesService;
+
+	@Autowired
+	private StockService stockService;
+
+	@Autowired
+	private PreProductOutService preProductOutService;
+
+	private PreProductOut preProductOut = new PreProductOut();
+
+	private Stock selectedStock = new Stock();
 
 	private Product selectedProduct = new Product();
 
@@ -66,8 +82,36 @@ public class MainView implements Serializable {
 	private void initStockList() {
 		Stock condition = new Stock();
 		condition.setProductId(selectedProduct.getId());
-		stockModel = new PaginationDataModel<Stock>(
-				"com.kxm.kcgl.mapper.StockMapper.selectSelective", condition);
+		stockModel = new PaginationDataModel<Stock>("com.kxm.kcgl.mapper.StockMapper.selectSelective", condition);
+	}
+
+	public void showPreProductOut(Integer stockId) {
+		Stock condition = new Stock();
+		condition.setId(stockId);
+		List<Stock> stocks = stockService.selectSelective(condition);
+		if (stocks.size() <= 0) {
+			MsgTool.addWarningMsg("库存不足");
+			return;
+		} else {
+			selectedStock = stocks.get(0);
+		}
+	}
+
+	public void preProductOut() {
+		try {
+			User user = (User) loginSession.getSesionObj();
+			preProductOut.setCreateUserId(user.getId());
+			boolean isOk = preProductOutService.insert(selectedStock, preProductOut);
+			if (isOk) {
+				MsgTool.addInfoMsg("预出货成功");
+				RequestContext.getCurrentInstance().execute("PF('pre_product_out').hide()");
+			} else {
+				MsgTool.addWarningMsg("预出货失败");
+			}
+		} catch (LogicException e) {
+			MsgTool.addWarningMsg(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	public Product getSelectedProduct() {
@@ -84,5 +128,21 @@ public class MainView implements Serializable {
 
 	public void setStockModel(PaginationDataModel<Stock> stockModel) {
 		this.stockModel = stockModel;
+	}
+
+	public PreProductOut getPreProductOut() {
+		return preProductOut;
+	}
+
+	public void setPreProductOut(PreProductOut preProductOut) {
+		this.preProductOut = preProductOut;
+	}
+
+	public Stock getSelectedStock() {
+		return selectedStock;
+	}
+
+	public void setSelectedStock(Stock selectedStock) {
+		this.selectedStock = selectedStock;
 	}
 }
